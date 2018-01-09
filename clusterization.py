cc13ec9b70917183d5ba2c2fe79e7d1e5e6ea4d4
Anthagonas <the_rod_21@hotmail.com>
2018-01-09 14:15:57 +0100

    added clusterization file

---
+++ clusterization.py
@@ -0,0 +1,14 @@
+# -*- coding : utf-8 -*-
+"""
+DataMining program : read 3 JSon files
+gather proteins' criteria values
+and creates clusters
+Json format :
+{ key1 :
+    {key2:,key3:,key4:,key5:,key6:,key7 :
+        [{key8:,key9 : [],key10:,key11:,key12:,key13:}]
+    }
+}
+key7 correspond to the domains (key name is "fasta")
+Key names may vary
+"""
