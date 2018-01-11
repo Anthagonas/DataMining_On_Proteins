b1d8798d704b64bb396a0cae899178cad3326ff7
Anthagonas <the_rod_21@hotmail.com>
2018-01-11 10:11:27 +0100

    finished the histogram R script

--- display_histogramme.R
+++ display_histogramme.R
@@ -1,4 +1,5 @@
 setwd("D:/Program Files (x86)/GitProject/DataMining_On_Proteins/")
-test<-read.csv("clusters.csv",sep=",")
-
-hist(as.numeric(test$protein_existence))
+#Loading the csv with all protein needed values
+data<-read.csv("clusters.csv",sep=",")
+#displaying histogram of the desired criteria (change the data$ value to display other histograms)
+hist(as.numeric(data$seq_length), main="histogram of the sequences length",xlab="sequence length (AA)")
