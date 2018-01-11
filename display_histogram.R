a98c3b742ee2029cd0a20816ef7b97f3a58d70d1
Amelie Laporte <amelie.lpe@sfr.fr>
2018-01-11 14:25:38 +0100

    update on display_histogram.R"

--- display_histogram.R
+++ display_histogram.R
@@ -1,5 +1,6 @@
-setwd("D:/Program Files (x86)/GitProject/DataMining_On_Proteins/")
+setwd("/net/cremi/amlaport/espaces/travail/M2_bioinfo/DEA/DataMining_On_Proteins/")
 #Loading the csv with all protein needed values
 data<-read.csv("clusters.csv",sep=",")
 #displaying histogram of the desired criteria (change the data$ value to display other histograms)
-hist(as.numeric(data$seq_length), main="histogram of the sequences length",xlab="length (in AA)")
+hist(as.numeric(data$seq_length), main="Histogram of the sequences length",xlab="length (in AA)",xlim=range(0,6000), labels = TRUE)
+help(hist)
