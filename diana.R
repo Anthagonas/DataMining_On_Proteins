97a4589c1697e495effcd1da2c41a490680dfe0e
Anthagonas <the_rod_21@hotmail.com>
2018-01-11 18:37:24 +0100

    first result of diana and agnes : sample of 3500 rows

--- diana.R
+++ diana.R
@@ -2,14 +2,19 @@ getwd()
 setwd("D:/Program Files (x86)/GitProject/DataMining_On_Proteins/")
 #install.packages("ggplot2")
 #install.packages("ggdendro")
+#install.packages("factoextra")
 library("cluster")
 library("ggplot2")
 library("ggdendro")
+library(factoextra)
 #Loading the csv with all protein needed values
-# change row names to "entry_name" ?
+#taking a sample from the datas
 data<-read.csv("clusters.csv",sep=",",row.names = 2)
 data<-subset(data,select = -seq)
 data<-subset(data,select = -name)
+data<-data[sample(nrow(data), 3500), ]
+
+#Top down Divisive analysis
 data_diana<-diana(data,FALSE)
 data_diana
 dg <- as.dendrogram(data_diana)
@@ -17,8 +22,10 @@ ddata <- dendro_data(data_diana)
 ggplot() + 
   geom_segment(data = ddata$segments, 
                aes(x = x, y = y, xend = xend, yend = yend))
+grp <- cutree(data_diana, k = 5)
+fviz_cluster(list(data = , cluster = grp))
 
-
+#Bottom up Agglomerative nesting
 data_agnes<-agnes(data,FALSE)
 data_agnes
 dg <- as.dendrogram(data_agnes)
