940eee0b35a37ec2ee037c3508b108fb03d278d0
Anthagonas <the_rod_21@hotmail.com>
2018-01-11 15:37:38 +0100

    updated diana script, NEED TO TEST IT

--- diana.R
+++ diana.R
@@ -1,25 +1,22 @@
+getwd()
 setwd("D:/Program Files (x86)/GitProject/DataMining_On_Proteins/")
-install.packages("ggplot2")
-install.packages("ggdendro")
+#install.packages("ggplot2")
+#install.packages("ggdendro")
 library("cluster")
 library("ggplot2")
 library("ggdendro")
 #Loading the csv with all protein needed values
 # change row names to "entry_name" ?
-data<-read.csv("clusters.csv",sep=",",row.names = 1)
+data<-read.csv("clusters.csv",sep=",",row.names = 2)
+data<-subset(data,select = -seq)
+data<-subset(data,select = -name)
 data_diana<-diana(data,FALSE)
 data_diana
 dg <- as.dendrogram(data_diana)
-ggdendrogram(dg,col=data$protein_existence)
-
 ddata <- dendro_data(data_diana)
 ggplot() + 
   geom_segment(data = ddata$segments, 
-               aes(x = x, y = y, xend = xend, yend = yend,colour = data$protein_existence))
-
-ggplot(data_diana)
-plot(data_diana,col=data$protein_existence)
-
+               aes(x = x, y = y, xend = xend, yend = yend))
 
 
 data_agnes<-agnes(data,FALSE)
